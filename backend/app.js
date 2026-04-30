5b505e53370cca9264f9d9c87d70352e9a161507
Mahdi Changizi <mchangizi10@gmail.com>
2026-04-30 10:58:03 +0200

    dbug1

--- backend/app.js
+++ backend/app.js
@@ -48,6 +48,6 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
 });
